303662a9e18c144fa5571d32aa6c676ace32e181
Esat Sefa Bayhan <esatbf@gmail.com>
2022-06-05 15:20:02 +0000

    Added manual pip install because rosdep fails

--- scripts/initial_build.sh
+++ scripts/initial_build.sh
@@ -5,3 +5,4 @@ pip install --upgrade pip
 pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U 
 rosdep update
 rosdep install --from-paths src --ignore-src -r -y
+pip install --upgrade torch torchvision tabulate
