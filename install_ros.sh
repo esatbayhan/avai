8df251c8aca6dc43a77eb59c9501cadc089c8523
Esat Sefa Bayhan <esatbf@gmail.com>
2022-06-05 13:53:11 +0000

    Changed workspace path & Added additional tools

--- install_ros.sh
+++ install_ros.sh
@@ -5,15 +5,16 @@
 # Original File: https://raw.githubusercontent.com/ROBOTIS-GIT/robotis_tools/master/install_ros2_foxy.sh
 
 echo ""
-echo "[Note] OS version  >>> Ubuntu 20.04 (Focal Fossa)"
-echo "[Note] Target ROS version >>> ROS 2 Foxy Fitzroy"
-echo "[Note] Colcon workspace   >>> $HOME/colcon_ws"
 echo "[Set the target ROS version and name of colcon workspace]"
 name_ros_version=${name_ros_version:="foxy"}
-name_colcon_workspace=${name_colcon_workspace:="colcon_ws"}
+path_colcon_workspace=${path_colcon_workspace:="/workspace/avai"}
+echo "[Note] OS version         >>> Ubuntu 20.04 (Focal Fossa)"
+echo "[Note] Target ROS version >>> ROS 2 Foxy Fitzroy"
+echo "[Note] Colcon workspace   >>> $path_colcon_workspace"
 
 echo "[Set Locale]"
-sudo apt update && sudo apt install locales
+sudo apt update
+sudo apt-get install -yq locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
@@ -24,26 +25,39 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -
 sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null'
 
 echo "[Install ROS 2 packages]"
-sudo apt update && sudo apt install -y ros-$name_ros_version-desktop
+sudo apt-get update
+sudo apt-get install -yq ros-$name_ros_version-desktop
 
 echo "[Environment setup]"
 source /opt/ros/$name_ros_version/setup.sh
-sudo apt install -y python3-argcomplete python3-colcon-common-extensions python3-vcstool git
+sudo apt-get install -yq \
+    python3-argcomplete \
+    python3-colcon-common-extensions \
+    python3-vcstool \
+    python3-rosdep2 \
+    git
 
-echo "[Make the colcon workspace and test colcon build]"
-mkdir -p $HOME/$name_colcon_workspace/src
-cd $HOME/$name_colcon_workspace
-colcon build --symlink-install
+echo "[Install ROS2 Tools]"
+sudo apt-get install -yq \
+    ros-foxy-gazebo-* \
+    ros-foxy-cartographer \
+    ros-foxy-cartographer-ros \
+    ros-foxy-navigation2 \
+    ros-foxy-nav2-bringup \
+    ros-foxy-dynamixel-sdk \
+    ros-foxy-turtlebot3-msgs \
+    ros-foxy-turtlebot3
 
 echo "[Set the ROS evironment]"
-sh -c "echo \"alias nb='nano ~/.bashrc'\" >> ~/.bashrc"
 sh -c "echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc"
-sh -c "echo \"alias gs='git status'\" >> ~/.bashrc"
-sh -c "echo \"alias gp='git pull'\" >> ~/.bashrc"
 
-sh -c "echo \"alias cw='cd ~/$name_colcon_workspace'\" >> ~/.bashrc"
-sh -c "echo \"alias cs='cd ~/$name_colcon_workspace/src'\" >> ~/.bashrc"
-sh -c "echo \"alias cb='cd ~/$name_colcon_workspace && colcon build --symlink-install && source ~/.bashrc'\" >> ~/.bashrc"
+sh -c "echo \"alias cw='cd $path_colcon_workspace'\" >> ~/.bashrc"
+sh -c "echo \"alias cs='cd $path_colcon_workspace/src'\" >> ~/.bashrc"
+sh -c "echo \"alias cb='cd $path_colcon_workspace && colcon build --symlink-install && source ~/.bashrc'\" >> ~/.bashrc"
 
 sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
-sh -c "echo \"source ~/$name_colcon_workspace/install/local_setup.bash\" >> ~/.bashrc"
+sh -c "echo \"source $path_colcon_workspace/install/local_setup.bash\" >> ~/.bashrc"
+
+sh -c "echo \"export ROS_DOMAIN_ID=30\" >> ~/.bashrc"
+
+
