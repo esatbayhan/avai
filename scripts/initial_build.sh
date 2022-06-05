4b83670fbc2cffec7aad08aec139a6c6d3807abe
Esat Sefa Bayhan <esatbf@gmail.com>
2022-06-05 15:09:29 +0000

    Added initial build

---
+++ scripts/initial_build.sh
@@ -0,0 +1,7 @@
+echo "[Build workspace]"
+source ~/.bashrc
+cd /workspace/avai
+pip install --upgrade pip
+pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U 
+rosdep update
+rosdep install --from-paths src --ignore-src -r -y
