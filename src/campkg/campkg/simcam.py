ad6ffe62dbf8858be362be3cc6bb45a3d45568f0
Esat Sefa Bayhan <esatbf@gmail.com>
2022-06-28 13:47:34 +0000

    I did it again - Added main version of campkg

--- src/campkg/campkg/simcam.py
+++ src/campkg/campkg/simcam.py
@@ -1,27 +1,30 @@
 import rclpy
+from cv_bridge import \
+    CvBridge  # Package to convert Image and CompressedImage
 from rcl_interfaces.msg import SetParametersResult
 from rclpy.node import Node
 from rclpy.parameter import Parameter
 from rclpy.publisher import Publisher
 from rclpy.subscription import Subscription
-from rclpy.timer import Timer
-from sensor_msgs.msg import CompressedImage  # Image is the message type
+from sensor_msgs.msg import CompressedImage, Image
 
 
-class Processing(Node):
+class Simcam(Node):
 
     def __init__(self):
-        super().__init__("campkg_processing")  # init node with name
-        self.subscriber_queue_size = 2
+        super().__init__('simcam')  # init node with name
+        self.image_format = "jpg"
+        self.encoding = "bgr8"
         self.publisher_queue_size = 2
+        self.subscriber_queue_size = 2
 
-        self.chached_frame = None
         self.publisher = None
 
         self.add_on_set_parameters_callback(self.init_parameters)
-        self.declare_parameter("subscriber_topic_name", "frames_raw")
+        self.declare_parameter('subscriber_topic_name', "camera/image_raw")
         self.declare_parameter("publisher_topic_name", "frames_processed")
-        self.declare_parameter("fps", 10)
+
+        self.cv_bridge = CvBridge()  # object to convert Image to CompressedImage
 
     def init_subscriber(self, topic_name: str) -> bool:
         # ToDo(0) topic names have naming specifications
@@ -33,33 +36,19 @@ class Processing(Node):
         if len(topic_name) == 0:
             return False
 
-        return type(self.create_subscription(CompressedImage, topic_name, self.subscribe, self.subscriber_queue_size)) == Subscription
+        return type(self.create_subscription(Image, topic_name, self.subscribe, self.subscriber_queue_size)) == Subscription
 
     def init_publisher(self, topic_name: str) -> bool:
-        # ToDo(0) topic names have naming specifications
-        # watch here https://design.ros2.org/articles/topic_and_service_names.html
-        # and make sure topic_name is a valid topic name
         if type(topic_name) != str:
             return False
 
         if len(topic_name) == 0:
             return False
-            
+
         self.publisher = self.create_publisher(
             CompressedImage, topic_name, self.publisher_queue_size)
+
         return type(self.publisher) == Publisher
-
-    def init_timer(self, fps: int) -> bool:
-        if type(fps) != int:
-            return False
-
-        for timer in self.timers:
-            self.destroy_timer(timer)
-
-        if fps <= 0:
-            return True
-
-        return type(self.create_timer(1 / fps, self.publish)) == Timer
 
     def initializer(self, param: Parameter, valid_types: tuple, init_function) -> bool:
         if param.type_ not in valid_types:
@@ -71,11 +60,7 @@ class Processing(Node):
         results = []
 
         for param in params:
-            if param.name == "fps":
-                results.append(self.initializer(
-                    param, (Parameter.Type.INTEGER,), self.init_timer))
-
-            elif param.name == "subscriber_topic_name":
+            if param.name == "subscriber_topic_name":
                 results.append(self.initializer(
                     param, (Parameter.Type.STRING,), self.init_subscriber))
 
@@ -89,28 +74,28 @@ class Processing(Node):
         successful = len(results) > 0 and all(results)
         return SetParametersResult(successful=successful)
 
-    def subscribe(self, msg: CompressedImage):
-        self.chached_frame = msg
+    def subscribe(self, image: Image) -> None:
+        # Image -> Numpy -> CompressedImage
 
-    def publish(self):
-        if type(self.publisher) != Publisher:
+        self.publish(
+            self.cv_bridge.cv2_to_compressed_imgmsg(
+                self.cv_bridge.imgmsg_to_cv2(image, self.encoding)))
+
+    def publish(self, compressed_image: CompressedImage) -> None:
+        if type(compressed_image) != CompressedImage:
             return
 
-        if type(self.chached_frame) != CompressedImage:
-            return
-
-        self.publisher.publish(self.chached_frame)
+        self.publisher.publish(compressed_image)
 
 
 def main(args=None):
 
     rclpy.init(args=args)
 
-    processing = Processing()
-    # spin node so callback function is called
-    rclpy.spin(processing)
+    simcam = Simcam()
+    rclpy.spin(simcam)  # spin node so callback function is called
 
-    processing.destroy_node()
+    simcam.destroy_node()
     rclpy.shutdown()
 
 
