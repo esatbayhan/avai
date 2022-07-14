bc15d590ea9a2626b110e3340a8bd55810160651
Esat Sefa Bayhan <esat.bayhan@rub.de>
2022-07-14 13:03:10 +0200

    Changed version number

--- src/controls/setup.py
+++ src/controls/setup.py
@@ -7,7 +7,7 @@ package_name = 'controls'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
